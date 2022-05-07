06520c08351d72bb0606c1a0da68571694f92204
Milena Michalska <milena.michalska@dac.digital>
2022-05-08 00:15:31 +0200

    WIP naive bayes classifier class

--- naive-bayes/naive-bayes.py
+++ naive-bayes/naive-bayes.py
@@ -9,31 +9,53 @@ from sklearn.model_selection import train_test_split
 # estimator class
 
 class NaiveBayesClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, laplacian_smoothing = False):
+    def __init__(self, n_bins = None, laplacian_smoothing = False):
         self.laplacian_smoothing = laplacian_smoothing
+        self.n_bins = n_bins
 
     def fit(self, X, y):
         # Check that X and y have correct shape
-        X, y = check_X_y(X, y)
+        # X, y = check_X_y(X, y)
+
+        # data discretization
+        if self.n_bins != None:
+            discretizer = KBinsDiscretizer(self.n_bins, encode='ordinal', strategy='uniform')
+            discretizer.fit(X)
+            X = discretizer.transform(X)
+
         # Store the classes seen during fit
-        self.classes_ = unique_labels(y)
-        self.features_ = X.shape[1]
-        print(self.features_)
+        self.classes = unique_labels(y)
+        self.features_count = X.shape[1]
         self.X_ = X
         self.y_ = y
 
         # Calculate classes a priori
         priori = []
-        for c in self.classes_:
+        for c in self.classes:
             priori.append((y == c).sum()/len(y))
         self.classes_priori = priori
 
-        # Calculate features likelihood
+        # Calculate features likelihood - multinominal distribution
         likelihood = []
-        for f in range(self.features_):
-            pass
+        for f in range(self.features_count):
+            value_row = []
+            for v in range(n_bins):
+                classes_row = []
+                for c in self.classes:
+                    #Calculated for every feature, for every value, for every class
+                    class_value_occ = 0
+                    for i, row in enumerate(X):
+                        if (row[f] == v and y[i] == c):
+                            class_value_occ += 1
+                    classes_row.append(class_value_occ/len(y))
+                value_row.append(classes_row)
+            likelihood.append(value_row)
+
+        likelihood = np.array(likelihood)
+        print(likelihood.shape)
+
         self.features_likelihood = likelihood
-          
+
         # Return the classifier
         return self
 
@@ -43,11 +65,49 @@ class NaiveBayesClassifier(BaseEstimator, ClassifierMixin):
         # Input validation
         X = check_array(X)
 
-        closest = np.argmin(euclidean_distances(X, self.X_), axis=1)
-        return self.y_[closest]
+        if self.n_bins != None:
+            discretizer = KBinsDiscretizer(self.n_bins, encode='ordinal', strategy='uniform')
+            discretizer.fit(X)
+            X = discretizer.transform(X)
+
+        results = []
+        for row in range(X.shape[0]):
+            class_prob = []
+            for c in range(len(self.classes)):
+                res = 1
+                for f in range(self.features_count):
+                    res *= self.classes_priori[c] * self.features_likelihood[f][int(X[row][f])][c]
+                class_prob.append(res)
+            results.append(class_prob)
+        
+        for r in results:
+            max = np.argmax(r)
+            r = np.where(r == max)
+
+        return results
 
     def predict_proba(self, X):
-        pass
+        # Check is fit had been called
+        check_is_fitted(self)
+        # Input validation
+        X = check_array(X)
+
+        if self.n_bins != None:
+            discretizer = KBinsDiscretizer(self.n_bins, encode='ordinal', strategy='uniform')
+            discretizer.fit(X)
+            X = discretizer.transform(X)
+
+        results = []
+        for row in range(X.shape[0]):
+            class_prob = []
+            for c in range(len(self.classes)):
+                res = 1
+                for f in range(self.features_count):
+                    res *= self.classes_priori[c] * self.features_likelihood[f][int(X[row][f])][c]
+                class_prob.append(res)
+            results.append(class_prob)
+
+        return results
 
 # config
 n_bins = 10 # number of bins to discretize data
@@ -55,19 +115,17 @@ n_bins = 10 # number of bins to discretize data
 # data loading
 data = np.genfromtxt('/home/milena/projects/studia/sztuczna-inteligencja/naive-bayes/wine.data', delimiter=",")
 print(data.shape)
-X = data[:,:13]
-y = data[:,-1:]
-
-# data discretization
-discretizer = KBinsDiscretizer(n_bins, encode='ordinal', strategy='uniform')
-discretizer.fit(X)
-Xt = discretizer.transform(X)
+X = data[:,1:]
+y = data[:,:1]
+# print(X.shape)
+# print(y.shape)
 
 # train test split
-X_train, X_test, y_train, y_test = train_test_split(Xt, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 # use classifier
-est = NaiveBayesClassifier()
-est.fit(X_train, y_train)
+est = NaiveBayesClassifier(n_bins)
+est = est.fit(X_train, y_train)
 prediction = est.predict(X_test)
+print(prediction)
 prediction_proba = est.predict_proba(X_test)

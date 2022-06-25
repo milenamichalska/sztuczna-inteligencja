e780eb28966c9a05f161bbe8e9875602ef774415
Milena Michalska <milena.michalska@dac.digital>
2022-06-25 23:22:03 +0200

    bfs working

--- breadth-first-search/bfs.py
+++ breadth-first-search/bfs.py
@@ -1,27 +1,40 @@
 import numpy as np
+import itertools
 
 class NQueens():
     def __init__(self, N):
         self.N = N
         self.queens_vector = []
 
-    def _generate_children_states(self):
-        starting_state = self.queens_vector
+    def _generate_children_states(self, starting_state):
         possible_states = []
         for i in range(self.N):
             possible_states.append(starting_state + [i])
         return possible_states
 
+    def _conditions_met(self, state):
+        conditions_met = True
+
+        if (len(state) < 4):
+            conditions_met = False
+
+        for x, y in itertools.combinations(enumerate(state), 2):
+            if (x[0] == y[0] or x[1] == y[1] or (np.abs(x[0] - y[0]) == np.abs(x[1] - y[1]))):
+                conditions_met = False
+
+        return conditions_met
+
     def solve(self):
-        for i in range(n):
-            states = self._generate_children_states()
-            for state in states:
-                conditions_met = True
-                if conditions_met:
-                    self.queens_vector = state
-                    break
+        states = self._generate_children_states([])
+        state_i = 0
+        while(len(states) > 0):
+            # print(states[state_i])
+            if (self._conditions_met(states[state_i])):
+                return states[state_i]
+            else:
+                states = states[1:] + self._generate_children_states(states[state_i])
         print(self.queens_vector)
 
 n = 4
 problem = NQueens(n)
-problem.solve()
+print(problem.solve())
